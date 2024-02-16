5f53586c9e6a4d3e0a1cbc24fcd4e159c75878cd
525santushevich <sophiaantushevich@icloud.com>
2024-02-16 10:09:45 -0500

    some css done

---
+++ script.js
@@ -0,0 +1,35 @@
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+  
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOptions = [rockBtn,paperBtn,scissorBtn];
+        const computerOptions = ['rock','paper','scissors']
+  
+  
+        playerOptions.forEach(option => {
+            option.addEventListener('click',function(){
+  
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10-moves}`;
+  
+  
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
+  
+  
+                winner(this.innerText,computerChoice)
+  
+  
+                if(moves == 10){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        })
+  
+    }
